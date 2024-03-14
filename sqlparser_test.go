6ad8bcbfc6417210b352b7fb6c89d90c63048cc2
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2024-03-14 23:13:07 +0700

    add simple test

--- sqlparser_test.go
+++ sqlparser_test.go
@@ -19,3 +19,33 @@ func TestLexSingleKeyword(t *testing.T) {
 		t.Errorf("TestLexSingleKeyword: unexpected tokens. expected=%+v, got=%+v", expected, tokens)
 	}
 }
+
+func TestLexIdentifier(t *testing.T) {
+	input := "tablename"
+	expected := []Token{
+		{Type: TokenIdentifier, Literal: "tablename"},
+		{Type: TokenEOF, Literal: ""},
+	}
+
+	lexer := NewLexer(input)
+	tokens := lexer.Lex()
+
+	if !reflect.DeepEqual(tokens, expected) {
+		t.Errorf("TestLexIdentifier: unexpected tokens. expected=%+v, got=%+v", expected, tokens)
+	}
+}
+
+// func TestLexSymbol(t *testing.T) {
+// 	input := "*"
+// 	expected := []Token{
+// 		{Type: TokenSymbol, Literal: "*"},
+// 		{Type: TokenEOF, Literal: ""},
+// 	}
+
+// 	lexer := NewLexer(input)
+// 	tokens := lexer.Lex()
+
+// 	if !reflect.DeepEqual(tokens, expected) {
+// 		t.Errorf("TestLexSymbol: unexpected tokens. expected=%+v, got=%+v", expected, tokens)
+// 	}
+// }
