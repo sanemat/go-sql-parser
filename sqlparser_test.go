10a03a6e6807729880ed00f6af2a99dba7772b9f
Matt (Sanemat) (Murahashi Kenichi) <o.gata.ken@gmail.com>
2024-03-14 23:07:23 +0700

    add test

--- sqlparser_test.go
+++ sqlparser_test.go
@@ -5,31 +5,17 @@ import (
 	"testing"
 )
 
-func TestLexer(t *testing.T) {
-	tests := []struct {
-		input    string
-		expected []Token
-	}{
-		{
-			"SELECT * FROM table;",
-			[]Token{
-				{Type: TokenKeyword, Literal: "SELECT"},
-				{Type: TokenSymbol, Literal: "*"},
-				{Type: TokenKeyword, Literal: "FROM"},
-				{Type: TokenIdentifier, Literal: "table"},
-				{Type: TokenSymbol, Literal: ";"},
-				{Type: TokenEOF, Literal: ""},
-			},
-		},
-		// Add more test cases here
+func TestLexSingleKeyword(t *testing.T) {
+	input := "SELECT"
+	expected := []Token{
+		{Type: TokenKeyword, Literal: "SELECT"},
+		{Type: TokenEOF, Literal: ""},
 	}
 
-	for _, tt := range tests {
-		lexer := NewLexer(tt.input)
-		tokens := lexer.Lex()
+	lexer := NewLexer(input)
+	tokens := lexer.Lex()
 
-		if !reflect.DeepEqual(tokens, tt.expected) {
-			t.Errorf("unexpected tokens. expected=%+v, got=%+v", tt.expected, tokens)
-		}
+	if !reflect.DeepEqual(tokens, expected) {
+		t.Errorf("TestLexSingleKeyword: unexpected tokens. expected=%+v, got=%+v", expected, tokens)
 	}
 }
